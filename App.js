f1cd140c86392647debe39f60392ef598fef4614
kimgeonsu <kundoo0412@gmail.com>
2021-10-28 23:09:26 +0900

    로그인 컴포넌트 개발중

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import LoginPage from './components/login/login-page';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LoginPage />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/login/login-page.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+function LoginPage() {
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+
+    const InputId = (input) => {
+        setId(input);
+    }
+    const InputPw = (input) => {
+        setPw(input);
+    }
+
+    const onLogin = () => {
+        console.log(id);
+        console.log(pw);
+    }
+    
+    return (
+        <View>
+            <Text style={styles.title}>매수</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="ID"
+                onChangeText={InputId}
+                value={id}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="password"
+                onChangeText={InputPw}
+                value={pw}
+            />
+
+            <TouchableOpacity activeOpacity={0.8} style={styles.btnLogin} onPress={onLogin} >
+                <Text style={styles.loginText}>Login</Text>
+            </TouchableOpacity> 
+
+            <Text style={styles.register}>회원가입</Text>   
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    title : {
+        color: '#207bd8',
+        fontSize: '20px;',
+        fontWeight: 'bolder',
+        textAlign: 'center',
+        marginBottom: 30
+    },
+    input : {
+        border: '1px solid #ddd',
+        borderRadius: 20,
+        padding: 10,
+        marginBottom: 10,
+    },
+    btnLogin : {
+        backgroundColor: '#207bd8',
+        marginTop: 30,
+        borderRadius: 20,
+        textAlign: 'center',
+        padding: 10 
+    },
+    loginText : {
+        color: '#fff',
+    },
+    register : {
+        textAlign: 'center',
+        marginTop: 10,
+        color: '#aaa',
+        fontSize: 12,
+    }
+})
+
+export default LoginPage;
