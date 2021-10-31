099d383e31f192d0ec6b0974e699f59d5813d29a
kimgeonsu <kundoo0412@gmail.com>
2021-10-31 23:52:41 +0900

    메인 페이지랑 랭킹 페이지 쪼꼼 퍼블리싱

--- components/home/home-page.js
+++ components/home/home-page.js
@@ -4,10 +4,16 @@ import { View, Text, TouchableOpacity  } from 'react-native';
 function HomePage() {
     const [time, setTime] = useState();
 
+
+    let interval;
     const startedTime = Date.now();
-    let start = () => {setInterval(
-        () =>  setTime(Date.now() - startedTime), 10, 
-    );}
+    let start =  () => {
+        interval =  setInterval(() => 
+            setTime(Date.now() - startedTime), 10);
+    }
+    let end = () => {
+        clearInterval(interval);
+    }
 
     return (
         <View>
@@ -15,6 +21,10 @@ function HomePage() {
             <TouchableOpacity
                 onPress={start}>
                     <Text>시작</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={end}>
+                    <Text>정지</Text>
             </TouchableOpacity>
         </View>
     );
---
+++ components/ranking/ranking-page.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'reat';
+import { View } from 'react-native';
+
+function RankingPage() {
+    
+    return (
+        <View>랭킹 페이지</View>
+    );
+}
+
+export default RankingPage;
