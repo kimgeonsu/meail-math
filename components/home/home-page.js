0acd0996ccf72dd60ebc9f7cd1a8755cedb56e16
kimgeonsu <49184941+kimgeonsu@users.noreply.github.com>
2021-11-24 09:46:43 +0900

    스탑워치 함수 생성

--- components/home/home-page.js
+++ components/home/home-page.js
@@ -1,33 +1,58 @@
-import React, { useState} from 'react'
+import React, { useState, useCallback } from 'react'
 import { View, Text, TouchableOpacity  } from 'react-native';
 
 function HomePage() {
-    const [time, setTime] = useState();
-
-
-    let interval;
-    const startedTime = Date.now();
-    let start =  () => {
-        interval =  setInterval(() => 
-            setTime(Date.now() - startedTime), 10);
-    }
-    let end = () => {
-        clearInterval(interval);
-    }
+    // let interval;
+    // const startedTime = Date.now();
+    // let start =  () => {
+    //     interval =  setInterval(() => 
+    //         setTime(Date.now() - startedTime), 10);
+    // }
+    // let end = () => {
+    //     clearInterval(interval);
+    // }
+    const { count, start, stop, reset } = useCounter(0, 1000);
 
     return (
         <View>
-            <Text>{time}</Text>
+            <Text>{count}</Text>
             <TouchableOpacity
                 onPress={start}>
                     <Text>시작</Text>
             </TouchableOpacity>
             <TouchableOpacity
-                onPress={end}>
+                onPress={stop}
+                >
                     <Text>정지</Text>
             </TouchableOpacity>
         </View>
     );
+}
+
+function useCounter(initialValue, ms) {
+    const [count, setCount] = useState(initialValue);
+    const intervalRef = useRef(null);
+    const start = useCallback(() => {
+        if (intervalRef.current !== null) {
+            return;
+        }
+        intervalRef.current = setInterval(() => {
+            setCount(c => c+1);
+        }, ms);
+    }, []);
+
+    const stop = useCallback(() => {
+        if (intervalRef.current === null) {
+            return;
+        }
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+    }, []);
+    const reset = useCallback(() => {
+        setCount(0);
+    }, []);
+
+    return { count, start, stop, reset };
 }
 
 export default HomePage;
