44477bd5897e6995926a71e84d177db7450b2ddc
kimgeonsu <kundoo0412@gmail.com>
2021-11-28 15:24:36 +0900

    프로필 페이지 생성

---
+++ components/profile/profile-page.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+
+function ProfilePage() {
+
+    return (
+        <View>
+            <Text>프로필 페이지</Text>
+        </View>
+    );
+}
+
+export default ProfilePage;
